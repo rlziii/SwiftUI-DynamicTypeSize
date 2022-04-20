50d1903beadc1301784f6fc3f83d7fd997df760e
Richard L Zarth III <rlziii@MS-18E.local>
2022-04-20 09:26:03 -0400

    Initial commit.

---
+++ FontSizes/FontSizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontSizesApp.swift
+//  FontSizes
+//
+//  Created by Richard L Zarth III on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct FontSizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
